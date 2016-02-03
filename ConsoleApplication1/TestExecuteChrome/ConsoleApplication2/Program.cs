ea3150c6669a509c0da9f0478b54ae1094781bc0
olaande <olaandersson93@gmail.com>
2016-02-03 14:58:47 +0100

    hej

---
+++ ConsoleApplication1/TestExecuteChrome/ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static Program fake;
+        public Program() {
+            System.Diagnostics.Process.Start("firefox.exe");
+
+        }
+        static void Main(string[] args)
+        {
+            fake = new Program();
+        }
+    }
+}
